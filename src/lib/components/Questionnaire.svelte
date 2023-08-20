2edf6809490562f7023b5775a3235d9e41490025
Christopher Roach <christopher.g.roach@gmail.com>
2023-08-20 15:55:30 -0700

    Clean up (and mobile first) CSS

--- src/lib/components/Questionnaire.svelte
+++ src/lib/components/Questionnaire.svelte
@@ -2,183 +2,146 @@
     export let questions;
 </script>
   
-<div class="centered" id= "questions">
-    <form name="questionnaire" method="POST">
-        {#each questions as { id, text, reversed }}
-        <fieldset>
-            <legend>{text}</legend>
-
-            <input type="radio" id="strongly_disagree_{id}" name="answer[{id}]" value="{reversed ? 5 : 1}">
-            <label for="strongly_disagree_{id}">
-            <span class="large-screen">Strongly disagree</span>
-            <span class="small-screen">1</span>
+<form class="questionnaire" name="questionnaire" method="POST">
+    {#each questions as { id, text, reversed }}
+    <fieldset>
+        <div class="question">{text}</div>
+        <div class="answers">
+            <input type="radio" id="likert-scale-{id}-1" name="answer[{id}]" value="{reversed ? 5 : 1}">
+            <label for="likert-scale-{id}-1">
+                <!-- <span class="large-screen">Strongly disagree</span> -->
+                <span>1</span>
             </label>
-
-            <input type="radio" id="disagree_{id}" name="answer[{id}]" value="{reversed ? 4 : 2}">
-            <label for="disagree_{id}">
-            <span class="large-screen">Disagree</span>
-            <span class="small-screen">2</span>
-            </label>
-            
-            <input type="radio" id="neither_agree_nor_disagree_{id}" name="answer[{id}]" value="{reversed ? 3 : 3}">
-            <label for="neither_agree_nor_disagree_{id}">
-            <span class="large-screen">Neither agree nor disagree</span>
-            <span class="small-screen">3</span>
+    
+            <input type="radio" id="likert-scale-{id}-2" name="answer[{id}]" value="{reversed ? 4 : 2}">
+            <label for="likert-scale-{id}-2">
+                <!-- <span class="large-screen">Disagree</span> -->
+                <span>2</span>
             </label>
             
-            <input type="radio" id="agree_{id}" name="answer[{id}]" value="{reversed ? 2 : 4}">
-            <label for="agree_{id}">
-            <span class="large-screen">Agree</span>
-            <span class="small-screen">4</span>
+            <input type="radio" id="likert-scale-{id}-3" name="answer[{id}]" value="{reversed ? 3 : 3}">
+            <label for="likert-scale-{id}-3">
+                <!-- <span class="large-screen">Neither agree nor disagree</span> -->
+                <span>3</span>
             </label>
             
-            <input type="radio" id="strongly_agree_{id}" name="answer[{id}]" value="{reversed ? 1 : 5}">
-            <label for="strongly_agree_{id}">
-            <span class="large-screen">Strongly agree</span>
-            <span class="small-screen">5</span>
+            <input type="radio" id="likert-scale-{id}-4" name="answer[{id}]" value="{reversed ? 2 : 4}">
+            <label for="likert-scale-{id}-4">
+                <!-- <span class="large-screen">Agree</span> -->
+                <span>4</span>
             </label>
-
-            <div class="small-screen alt-labels">
-            <span>Strongly Disagree</span>
-            <span>Strongly Agree</span>
-            </div>
-
-        </fieldset>
-        {/each}
-
-        <div class="submit-button">
-        <button>Submit</button>
+            
+            <input type="radio" id="likert-scale-{id}-5" name="answer[{id}]" value="{reversed ? 1 : 5}">
+            <label for="likert-scale-{id}-5">
+                <!-- <span class="large-screen">Strongly agree</span> -->
+                <span>5</span>
+            </label>    
         </div>
 
-    </form>
-</div>
+        <div class="likert-scale-labels">
+            <span>Strongly Disagree</span>
+            <span>Strongly Agree</span>
+        </div>
+
+    </fieldset>
+    {/each}
+
+    <div class="submit-button">
+        <button>Submit</button>
+    </div>
+
+</form>
   
 <style>
-.centered {
-    display: flex;
-    justify-content: center;
-}
 
-#questions {
-    padding-top: 50px;
-}
+form {
+    /* Custom CSS variables */
+    --button-background-color: #eeeded;
+    --button-label-color: rgb(103, 102, 102);
+    --button-checked-background-color: #5fc6f5;
+    --button-checked-label-color: rgb(255, 255, 255);
 
-.submit-button {
-    text-align: center;
-    padding-bottom: 100px;
+    /* Add a space between the questions and the top and bottom of the "page" */
+    margin: 20px 0 20px 0;
 }
 
 fieldset {
+    /* Remove the standard border on the fieldset */
     border: 0;
-    padding: 0 20px 100px 20px;
+    /* Remove the standard padding on the fieldset */
+    padding: 0;
+    /* Add some space between each question */
+    margin-bottom: 100px; 
+}
+
+div.question {
+    /* Add a little padding between the question and the sides of the screen */
+    padding: 0 10px 0 10px;
+    /* Center the question */
+    text-align: center;
+    /* Add a bit of space between the question and the answers */
+    margin-bottom: 10px;
+}
+
+div.answers {
+    /* Create a Likert scale set of answers */
     display: grid;
-    max-width: 900px;
-    grid-auto-rows: 1fr;
+    grid-template-columns: repeat(5, 1fr);
+    /* Add some space between each of the answers */
     gap: 2px;
-    grid-template-columns: repeat(5, minmax(0, 1fr));
-}
-
-fieldset > legend {
-    padding: 0 0px 30px 0px;
-}
-
-div.alt-labels {
-    display: flex;
-    width: 100vw;
-    justify-content: space-between;
-}
-
-div.alt-labels > span {
-    padding: 7px 10px 0 10px;
-    font-size: 14px;
-}
-
-.large-screen {
-    visibility: visible;
-    display: inline;
-}
-
-.small-screen {
-    visibility: hidden;
-    display: none;
-}
-
-input {
-    /* max-width: 250px; */
-    position: fixed;
-    opacity: 0;
-    pointer-events: none;
 }
 
 label {
-    /* border-radius: 5px; */
-    display: flex;
-    justify-content: center;
-    align-items: center;
+    font-size: 12px;
+    color: var(--button-label-color);
+    background: var(--button-background-color);
+    /* Center the label */
     text-align: center;
-    box-sizing: border-box;
-    width: 100%;
-    height: 100%;
-    padding: 10px;
-    background: #eeeded;
-    color: rgb(103, 102, 102)
-    /* transition: background .2s ease-in-out; */
+    /* Add a bit of padding around the labels. The padding is relative to the
+       the font size for the labels. */
+    padding: 1em;
+}
+
+input {
+    /* Hide the radio button */
+    opacity: 0;
+    /* By default, the label (1, 2, 3, etc.) is displayed to the right of the
+       radio button. Setting its position to "fixed" positions it relative to 
+       its containing block, i.e., the label element. If you don't set the 
+       position in this way, the label will show up slightly to the right of 
+       center. */
+    position: fixed;
 }
 
 input:checked + label {
-    /* background: #18adf3; */
-    /* opacity: 0.9; */
-    background: #5fc6f5;
-    color: rgb(255, 255, 255);
+    /* Highlight selected answer by changing the background and text colors */
+    background: var(--button-checked-background-color);
+    color: var(--button-checked-label-color);
 }
 
-input:focus + label {
-    /* outline-style: dotted; */
+div.likert-scale-labels {
+    font-size: 12px;
+    /* Push the labels to opposite ends of the scale */
+    display: flex;
+    justify-content: space-between;
+    /* Add a bit of padding between the labels and the edges of the screen */
+    padding: 0 5px 0 5px;
+    /* Add a bit of space between the top of the labels and answers */
+    margin-top: 3px;
 }
 
-@media (hover: hover) {
-    label:hover {
-        background: #fbfafa;
-    }
+div.submit-button {
+    /* Center the submit button horizontally */
+    display: flex;
+    justify-content: center;
 }
 
 button {
-    background-color: #eeeded;
+    background-color: var(--button-background-color);
     border: none;
-    padding: 16px 32px;
-    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
-    font-size: 20px;
-    text-rendering: optimizeLegibility;
-    color: rgb(103, 102, 102)
+    padding: 1em 2em;
+    font-size: 16px;
+    color: var(--button-label-color);
 }
 
-button:hover {
-    background-color: #5fc6f5; 
-    cursor: pointer;
-}   
-
-
-@media only screen and (max-width: 680px) {
-
-    .large-screen {
-        visibility: hidden;
-        display: none;
-    }
-
-    .small-screen {
-        visibility: visible;
-        display: inline;
-    }
-
-    fieldset {
-        text-align: center;
-        padding-left: 0;
-        padding-right: 0;
-    }
-
-    fieldset > legend {
-        padding: 0 40px 30px 40px;
-    }
-
-}
 </style>
