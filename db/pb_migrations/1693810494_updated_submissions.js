190a5d7928220abb10e6b2f71e8987edec143ef9
Christopher Roach <christopher.g.roach@gmail.com>
2023-09-04 11:01:57 -0700

    Add permissions to create and view submissions

---
+++ db/pb_migrations/1693810494_updated_submissions.js
@@ -0,0 +1,16 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("69m51uzj362qt3l")
+
+  collection.viewRule = ""
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("69m51uzj362qt3l")
+
+  collection.viewRule = null
+
+  return dao.saveCollection(collection)
+})
