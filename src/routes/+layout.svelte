60270571acd3ac332bac480bc60d033b98d240b6
Christopher Roach <christopher.g.roach@gmail.com>
2023-09-04 12:15:05 -0700

    Move common CSS into main.css

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,27 +1,4 @@
+<!-- Add a "main" div to all pages to make sure the content is aligned properly-->
 <div class="main">
     <slot></slot>
 </div>
-
-<style>
-div.main {
-    --left-right-margins: 0.8rem;
-
-    /* Maker sure that the form looks good on bigger screens */
-    max-width: 900px;
-}
-
-h1, h2 {
-    /* Add some space around the title  */
-    margin: 1rem var(--left-right-margins);
-}
-
-h2 {
-    font-size: 1em;
-    font-weight: bold;
-}
-
-p {
-    /* Add some space around the paragraphs */
-    margin: 1rem var(--left-right-margins);
-}
-</style>
