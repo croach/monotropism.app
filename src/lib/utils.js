29efed6d1c1d28fbfa7a8b5c20a4d563f067ef67
Christopher Roach <christopher.g.roach@gmail.com>
2023-09-30 14:15:41 -0700

    Bug fix for the shuffle function
    The shuffle function was not previously shuffling everything in the
    array with the exception of the last item. This has been fixed.

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,5 +1,5 @@
 export function shuffle(arr) {
-    for (let i = 0; i < arr.length - 1; i++) {
+    for (let i = 0; i < arr.length; i++) {
         const j = Math.floor(Math.random() * i);
         [arr[j], arr[i]] = [arr[i], arr[j]];
     }
