111c2e14c99faf7181ae3bbd544680c6d837c05c
Christopher Roach <christopher.g.roach@gmail.com>
2023-09-30 17:10:59 -0700

    Switch to /dynamic to read env variables

--- src/lib/pocketbase.js
+++ src/lib/pocketbase.js
@@ -1,4 +1,4 @@
 import PocketBase from 'pocketbase';
-import { POCKETBASE_URI } from '$env/static/private';
+import { POCKETBASE_URI } from '$env/dynamic/private';
 
 export const pb = new PocketBase(POCKETBASE_URI);
