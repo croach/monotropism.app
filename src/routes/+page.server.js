268f63a8b719eaa561c49de6132992e2eb4f425e
Christopher Roach <christopher.g.roach@gmail.com>
2023-10-11 09:30:35 -0700

    Add code to extract answers from form data

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -25,8 +25,10 @@ export const actions = {
 
         // Pull the answers out of the form data and format the data into an object with the question number as the key 
         // and the answer as the value (e.g., { "1": 5, "2": 4, ... }})
-        const questions = Array.from(formData.entries())
-        const submission = questions.reduce( 
+        const survey = Array.from(formData.entries())
+        const answers = survey
+            .filter(([key, ]) => key.startsWith('answer'))
+            .reduce( 
                 (obj, [num, val]) => (obj[num.slice(7, -1)] = parseInt(val), obj), 
                 {}
             )
@@ -34,13 +36,13 @@ export const actions = {
         // If the user chose to save the data, create a new submission and redirect to them to the results page
         if (saveData) {
             // Stringify the submission so we can store it in the database
-            const data = { submission: JSON.stringify(submission) }
+            const data = { submission: JSON.stringify(answers) }
             const record = await pb.collection('submissions').create(data)
             throw redirect(302, `${record.id}`)
         } 
         
         // Otherwise, just return the data
-        const { monotropismScore, maxMonotropismScore, avgMonotropismScore } = calcMonotropismScore(submission);
+        const { monotropismScore, maxMonotropismScore, avgMonotropismScore } = calcMonotropismScore(answers);
         const stats = getQuestionnaireStats();
         return {
             ...stats,
